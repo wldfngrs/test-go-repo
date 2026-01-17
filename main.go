e35d8bc2e4ed82838a6424a12f8fec26a6631abd
wldfngrs <wldfngrs@gmail.com>
2026-01-17 12:51:59 +0100

    update code

--- main.go
+++ main.go
@@ -4,8 +4,14 @@ import "fmt"
 
 func main() {
   sum := Add(7, 8)
+  sub := Sub(8, 7)
   fmt.Printf("%d", sum)
+  fmt.Printf("%d", sub)
   fmt.Printf("Test statement 121")
+}
+
+func Sub(a int, b int) int {
+  return a - b
 }
 
 func Add(a int, b int) int {
